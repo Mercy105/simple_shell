a44c86ae92af46bdbaf3a678181d3ae4ced7c664
Donnelly <donamaitsa@gmail.com>
2023-08-16 10:07:44 -0700

    Command line interpreter fixed

--- commandinterpreter.c
+++ commandinterpreter.c
@@ -1,127 +1,80 @@
 #include "shell.h"
-
-int cant_open(char *file_path);
-int proc_file_commands(char *file_path, int *exe_ret);
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#define MAX_LENGTH 100
 /**
- * cant_open - If the file doesn't exist or lacks proper permissions, print
- * a cant open error.
- * @file_path: Path to the supposed file.
- * Return: 127.
+ * display_prompt - used to display the prompt
  */
-
-int cant_open(char *file_path)
+void display_prompt(void)
 {
-	char *error, *hist_str;
-	int len;
-	
-	hist_str = _itoa(hist);
-	
-	if 
-		(!hist_str)
-		return (127);
-	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
-	error = malloc(sizeof(char) * (len + 1));
-	
-	if (!error)
-	{
-		free(hist_str);
-		return (127);
-	}
-	_strcpy(error, name);
-	_strcat(error, ": ");
-	_strcat(error, hist_str);
-	_strcat(error, ": Can't open ");
-	_strcat(error, file_path);
-	_strcat(error, "\n");
-	free(hist_str);
-	write(STDERR_FILENO, error, len);
-	free(error);
-	return (127);
+	printf("simple_shell$ ");
+	fflush(stdout);
 }
-
 /**
- * proc_file_commands - Takes a file and attempts to run the commands stored
- * within.
- * @file_path: Path to the file.
- * @exe_ret: Return value of the last executed command.
- * Return: If file couldn't be opened - 127.
- *	   If malloc fails - -1.
- *	   Otherwise the return value of the last command ran.
+ * execute_command - carries out execution of commands
+ * @command: argument
  */
-int proc_file_commands(char *file_path, int *exe_ret)
+void execute_command(char *command)
 {
-	ssize_t file, b_read, i;
-	unsigned int line_size = 0;
-	unsigned int old_size = 120;
-	char *line, **args, **front;
-	char buffer[120];
-	int ret;
-	
-	hist = 0;
-	file = open(file_path, O_RDONLY);
-	
-	if (file == -1)
-	{
-		*exe_ret = cant_open(file_path);
-		return (*exe_ret);
-	}
+	pid_t pid = fork();
 
-	line = malloc(sizeof(char) * old_size);
-	if 
-		(!line)
-		return (-1);
-	do
+	if (pid == -1)
 	{
-		b_read = read(file, buffer, 119);
-	if
-		(b_read == 0 && line_size == 0)
-		return (*exe_ret);
-	buffer[b_read] = '\0';
-	line_size += b_read;
-	line = _realloc(line, old_size, line_size);
-	_strcat(line, buffer);
-	old_size = line_size;
+		perror("fork");
+		exit(EXIT_FAILURE);
 	}
-	while
-		(b_read);
-	for (i = 0; line[i] == '\n'; i++)
-		line[i] = ' ';
-	for (; i < line_size; i++)
+	if (pid == 0)
 	{
-		if (line[i] == '\n')
+		char *args[] = {NULL, NULL};
+
+		args[0] = command;
+		execve(args[0], args, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+/**
+ * rum_shell - program to run the shell
+ */
+void run_shell(void)
+{
+	char buffer[100];
+	char command[100];
+
+	size_t len = strlen(buffer);
+	while (1)
+	{
+		display_prompt();
+		if (fgets(buffer, sizeof(buffer), stdin) == NULL)
 		{
-			line[i] = ';';
-			for (i += 1; i < line_size && line[i] == '\n'; i++)
-				line[i] = ' ';
+			if (feof(stdin))
+			{
+				printf("\nExiting simple_shell\n");
+				break;
+			}
+			else
+			{
+				perror("fgets");
+				exit(EXIT_FAILURE);
+			}
 		}
+		if (len > 0 && buffer[len - 1] == '\n')
+		{
+			buffer[len - 1] = '\0';
+		}
+		if (strlen(buffer) == 0)
+		{
+			continue;
+		}
+		strcpy(command, buffer);
+		execute_command(buffer);
 	}
-	variable_replacement(&line, exe_ret);
-	handle_line(&line, line_size);
-	args = _strtok(line, " ");
-	free(line);
-	
-	if (!args)
-		return (0);
-	if (check_args(args) != 0)
-	{
-		*exe_ret = 2;
-		free_args(args, args);
-		return (*exe_ret);
-	}
-front = args;
-for (i = 0; args[i]; i++)
-{
-if (_strncmp(args[i], ";", 1) == 0)
-{
-free(args[i]);
-args[i] = NULL;
-ret = call_args(args, front, exe_ret);
-args = &args[++i];
-i = 0;
-}
-}
-ret = call_args(args, front, exe_ret);
-free(front);
-return (ret);
 }
