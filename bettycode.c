14a7ca525dd0ad7eb480c995c717103911908383
Donnelly <donamaitsa@gmail.com>
2023-08-23 03:23:39 -0700

    fixing betty error

---
+++ bettycode.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * add_numbers - used to add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Always 0 (success)
+ */
+int add_numbers(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	printf("Sum is %d", sum);
+	return (0);
+}
