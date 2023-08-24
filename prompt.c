d9122d3502044ce7d41635fdad1e50d80a051778
Donnelly <donamaitsa@gmail.com>
2023-08-24 02:53:19 -0700

    fixed

--- prompt.c
+++ prompt.c
@@ -1,18 +1,19 @@
 #include "shell.h"
+#include <dirent.h>
 /**
  * display_prompt - used to display shell prompt
  * Return: Standard output
  */
 void display_prompt(void)
 {
-	printf("$ ");
 	DIR *d;
 	struct dirent *dir;
 
+	printf("$ ");
 	d = opendir(".");
 	if (d)
 	{
-		while ((dir = readir(d)) != NULL)
+		while ((dir = readdir(d)) != NULL)
 		{
 			printf("%s\n", dir->d_name);
 		}
