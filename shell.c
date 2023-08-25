ec346c56a11b35340d69576a37ce19723281f419
Mercy105 <wawerumercy105@gmail.com>
2023-08-25 05:18:52 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,12 +1,11 @@
 #include "shell.h"
-
 /**
  * main - entry point
  * @argc: argument input
  * @argv: argument input
  * Return: always 0
 */
-int main( int  argc, char *argv[])
+int main(__attribute__ int  argc, char *argv[])
 {
 	signal(SIGINT, &new_line);
 	shell(argv[0]);
